87b0f4029b0673c992f264ec8da515e6c4ccc388
Emil Kristian Rasmussen <1089450@gmail.com>
2023-01-28 14:44:16 +0100

    Removed old projects and added comments

--- FormDataReciever/Controllers/FileController.cs
+++ FormDataReciever/Controllers/FileController.cs
@@ -15,17 +15,28 @@ namespace FormDataReciever.Controllers
 
         [HttpPost]
         [Route("upload")]
-        public IActionResult UploadFiles([FromForm] IEnumerable<IFormFile> files)
+        public async Task<IActionResult> UploadFiles([FromForm] IEnumerable<IFormFile> files)
         {
-            Console.WriteLine("hello");
+            //Starting a loop iterating through all the files we recieved
             foreach(var file in files)
             {
-                Console.WriteLine("found a file");
                 Console.WriteLine(file.FileName);
 
+                //In our first use case, we will only be doing things if the file type is of a spreadsheet
                 if(Path.GetExtension(file.FileName) == ".xlsx")
                 {
-                    Console.WriteLine("YES EXCEL!");
+                    //Making a file path to our current directory along with the filename of the file
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), file.FileName);
+                    
+                    //Here we create a fileStream to save it to desk at the filepath location
+                    using(var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                        Console.WriteLine("File should now be saved");
+                    }
+
+                    //From here on out, we can open the sheet with EPPlus, manipulate and do whatever we want with the data
+                    //Do remember to remove the temporary file if you dont want it saved on the server in the future
                 }
             }
 
--- FormDataSender/Program.cs
+++ FormDataSender/Program.cs
@@ -1,46 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Net.Http.Headers;
-Console.ReadKey();
-Console.WriteLine("Hello, World!");
 
+// A Path to a file i want to send
 string path1 = "C:\\Users\\rainf\\Desktop\\testsheet.xlsx";
+
+
+//API URL
 string url = "https://localhost:7185/api/File/upload";
 
-//using (var client = new HttpClient())
-//{
-//    using (var content = new MultipartFormDataContent())
-//    {
-//        // Add the files to the content
-//        var file1 = new ByteArrayContent(File.ReadAllBytes(path1));
-//        file1.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
-//        {
-//            FileName = "testsheet.xlsx"
-//        };
-//        content.Add(file1);
-
-//        // Send the request
-//        var result = await client.PostAsync(url, content);
-//        Console.WriteLine(result);
-//    }
-//}
+//Making a list of all the files we want to send
+//I realize i only have one path already added, but this is to illustrate how to send more than one at a time
 List<string> files = new List<string>();
 files.Add(path1);
-files.Add(path1);
+
 using (var httpClient = new HttpClient())
 {
     using (var formData = new MultipartFormDataContent())
     {
         foreach (var file in files)
         {
+            //Opens a file stream with the file
             var fileStream = File.OpenRead(file);
+
+            //StreamContent makes a HTTP Content based on a stream (filestream in this case)
             var fileContent = new StreamContent(fileStream);
+
+            //Adding the content header saying its form data. and then also adding metadata
+            //We will use the FileName on the server to see how we should handle the file based on the extension
             fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
             {
                 Name = "files",
                 FileName = Path.GetFileName(file)
             };
+            //Add it to our formdata
             formData.Add(fileContent);
         }
+        //Send the form data through to the API
         var response = await httpClient.PostAsync(url, formData);
     }
 }
